3844864a73c6e0939ccaf256508960d3346b78e4
liyuxin <liyuxin6@xiaomi.com>
2020-07-17 15:55:46 +0800

    feat: 增加新功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
       <el-table-column prop="address" label="地址"></el-table-column>
       <el-table-column prop="address" label="啦啦啦"></el-table-column>
       <el-table-column prop="address" label="啦啦啦"></el-table-column>
+      <el-table-column prop="address" label="啦啦啦"></el-table-column>
     </el-table>
   </div>
 </template>
