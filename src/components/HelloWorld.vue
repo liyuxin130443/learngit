8d6860794e1baec2034a90eee1f7f2b98fd6b502
liyuxin <liyuxin6@xiaomi.com>
2020-07-22 16:07:03 +0800

    feat: 测试5
    测试一下

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,6 +9,7 @@
       <el-table-column prop="address" label="啦啦啦"></el-table-column>
       <el-table-column prop="address" label="啦啦啦"></el-table-column>
       <el-table-column prop="address" label="啦啦啦"></el-table-column>
+      <el-table-column prop="address" label="啦啦啦"></el-table-column>
     </el-table>
   </div>
 </template>
